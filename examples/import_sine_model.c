884f1ad63735e786d127036f076aedb9acffd546
Logan Lucas <logan.lucas83@gmail.com>
2024-09-30 20:14:38 +0200

    one example to train and save, other to load and plot

--- examples/import_sine_model.c
+++ examples/import_sine_model.c
@@ -1,41 +1,12 @@
 #include <matrix.h>
-#include <vector.h>
 #include <neural_network.h>
 #include <utils.h>
-#include <model_trainer.h>
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-
-#define INPUT_SIZE 1
-#define OUTPUT_SIZE 1
-#define DATASET_SIZE 1000
-
-double f(double x) {
-    return (sin(20.0 * x) + 1) / 2.0;
-}
 
 int main(void) {
-    srand(43);
-
-    // Create dataset
-    double data[DATASET_SIZE][INPUT_SIZE];
-    double output_data[DATASET_SIZE][OUTPUT_SIZE];
-    for (size_t i = 0; i < DATASET_SIZE; i++) {
-        data[i][0] = (double) i / DATASET_SIZE;
-        output_data[i][0] = f(data[i][0]);
-    }
-
-    NeuralNetwork* nn = new_neural_network(INPUT_SIZE, 10, OUTPUT_SIZE);
+    NeuralNetwork* nn = new_neural_network_from_file("sine_model.ncn");
     set_activation_functions(nn, sigmoid, sigmoid_derivative);
-    ModelTrainer trainer;
-    trainer.nn = nn;
-    trainer.learning_rate = 0.8;
-    trainer.batch_size = 10;
-    trainer.epochs = 10000;
-
-    double* loss_history = train_with_history(&trainer, data[0], output_data[0], DATASET_SIZE);
 
     Matrix* input = new_uninitialized_matrix(1, 10);
     set_batch_size(nn, 10);
@@ -57,7 +28,6 @@ int main(void) {
     fprintf(gnuplot, "e\n");
     fflush(gnuplot);
 
-    free(loss_history);
     free_matrix(input);
     free_neural_network(nn);
 
---
+++ examples/sine.c
@@ -0,0 +1,42 @@
+#include <neural_network.h>
+#include <utils.h>
+#include <model_trainer.h>
+
+#include <stdlib.h>
+#include <math.h>
+
+#define INPUT_SIZE 1
+#define OUTPUT_SIZE 1
+#define DATASET_SIZE 1000
+
+double f(double x) {
+    return (sin(20.0 * x) + 1) / 2.0;
+}
+
+int main(void) {
+    srand(43);
+
+    // Create dataset
+    double data[DATASET_SIZE][INPUT_SIZE];
+    double output_data[DATASET_SIZE][OUTPUT_SIZE];
+    for (size_t i = 0; i < DATASET_SIZE; i++) {
+        data[i][0] = (double) i / DATASET_SIZE;
+        output_data[i][0] = f(data[i][0]);
+    }
+
+    NeuralNetwork* nn = new_neural_network(INPUT_SIZE, 10, OUTPUT_SIZE);
+    set_activation_functions(nn, sigmoid, sigmoid_derivative);
+    ModelTrainer trainer;
+    trainer.nn = nn;
+    trainer.learning_rate = 0.8;
+    trainer.batch_size = 10;
+    trainer.epochs = 10000;
+
+    train(&trainer, data[0], output_data[0], DATASET_SIZE);
+
+    save_neural_network(nn, "sine_model.ncn");
+
+    free_neural_network(nn);
+
+    return 0;
+}
